254bccf3145715c44390e8f3537ef8470dff1592
Trevon Fernando <trevonfernando@MacBook-Pro.local>
2016-09-16 09:42:18 +0200

    First Commit. project initializer

---
+++ DTUSenseCore/DTUSenseCore.h
@@ -0,0 +1,19 @@
+//
+//  DTUSenseCore.h
+//  DTUSenseCore
+//
+//  Created by Trevon Fernando on 9/16/16.
+//  Copyright © 2016 DTU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DTUSenseCore.
+FOUNDATION_EXPORT double DTUSenseCoreVersionNumber;
+
+//! Project version string for DTUSenseCore.
+FOUNDATION_EXPORT const unsigned char DTUSenseCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DTUSenseCore/PublicHeader.h>
+
+
